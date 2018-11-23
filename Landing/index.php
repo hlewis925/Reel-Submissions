6a4a469fc9d6551c0bbaff4c0edbe130940dabc5
hlewis925 <hlewis925@gmail.com>
2018-11-22 23:27:10 -0600

    Add files via upload

---
+++ Landing/index.php
@@ -0,0 +1,39 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Reel Submissions</title>
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Antic" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+
+</head>
+
+<body>
+	<div id="header">
+		<div id="leftside"><img src="logo.jpg"></div>
+		<div class="rightside">
+			<h1 class="title">Reel</h1>
+			<br>
+			<h1 class="title">Submissions</h1>
+		</div>
+    </div>
+<div id="wrapper">
+	<div id="content">
+	<h3>Coming to you soon</h3>
+	<h3>a platform by creatives for creatives</h3>
+	<p>We're a group of problem solvers bringing forward a gateway for artists and filmmakers. As artists and creator ourselves, we know the challenge of trying to get <em>your work, your message, and your story </em>out into the world</p>
+	<p>To the creative, the diligent, the beginners, the experts, the censored the curious, the emotional and the insatiable; this is the place for you</p>
+	 <form action="thank.php" method="post" id="newsletter" name="newsletter">
+		<input type="email" name="signup-email" id="signup-email" value="" placeholder="Insert email here" />
+		 <button type="submit" value="Subscribe" name="signup-button" id="signup-button">Sign Up</button>
+		<span class="arrow"></span>
+	</form>
+	<div id="response"></div>
+	</div>
+
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="assets/js/lib.js"></script>
+</div>
+</body>
+</html>
